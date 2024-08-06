92b4a07dde955bab1bf243eb02db2bd999c5341f
kurokobo <2920259+kurokobo@users.noreply.github.com>
2024-08-06 15:32:44 +0000

    fix: add lower-cased proxy envs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
+export http_proxy=${HTTP_PROXY}
+export https_proxy=${HTTPS_PROXY}
+
 if [ -f /dependencies/apt-requirements.txt ]; then
   apt-get update
   apt-get install -y $(cat /dependencies/apt-requirements.txt)
